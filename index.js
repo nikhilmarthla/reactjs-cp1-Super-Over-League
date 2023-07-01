ab0cd77f5e3bd7cde9c1606df9f75ac8efa5b9c1
root <nikhil.marthala@gmail.com>
2023-07-01 08:01:08 +0000

    reactjs-cp1-Super-Over-League

---
+++ index.js
@@ -0,0 +1,10 @@
+const element = (
+  <div className="bgContainer">
+    <h1 className="heading">Super Over League</h1>
+    <div className="imagesContainer">
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+    </div>
+  </div>
+
+ReactDOM.render(element, document.getElementById('root'))
